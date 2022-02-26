29abce84c0458388a77161be797699e83308fc57
Erik-Handeland <erikhandeland@gmail.com>
2022-02-25 18:14:13 -0800

    Library view: list mode

--- app/src/main/java/com/example/movietime/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/example/movietime/ui/library/LibraryFragment.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movietime.R
+import com.example.movietime.data.Movie
 import com.example.movietime.databinding.FragmentLibraryBinding
+import com.example.movietime.ui.home.MovieListAdapter
 
 class LibraryFragment : Fragment() {
 
     private var _binding: FragmentLibraryBinding? = null
+    private val movieAdapter = MovieListAdapter()
+    private lateinit var searchResultsListRV: RecyclerView
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,10 +34,11 @@ class LibraryFragment : Fragment() {
         _binding = FragmentLibraryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textLibrary
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        searchResultsListRV = root.findViewById(R.id.rv_library_list)
+        searchResultsListRV.layoutManager = LinearLayoutManager(root.context)
+        searchResultsListRV.adapter = movieAdapter
+        movieAdapter.updateMovieList(listOf(Movie("Shrek is an anti-social and highly-territorial green ogre who loves the solitude of his swamp. His life is interrupted after the dwarfish Lord Farquaad of Duloc unknowingly exiles a vast number of fairy-tale creatures to Shrek's swamp.",93.0,"2000","Shrek")))
+
         return root
     }
 
@@ -39,4 +46,5 @@ class LibraryFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
 }
