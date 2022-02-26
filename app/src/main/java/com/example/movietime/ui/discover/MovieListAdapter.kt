0bce6443498528e0f29708636f489ec53ad85128
Erik-Handeland <erikhandeland@gmail.com>
2022-02-25 20:53:51 -0800

    Added grid view

--- app/src/main/java/com/example/movietime/ui/discover/MovieListAdapter.kt
+++ app/src/main/java/com/example/movietime/ui/discover/MovieListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.movietime.ui.home
 
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movietime.data.Movie
 import android.view.LayoutInflater
@@ -8,7 +9,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.example.movietime.R
 
+object MovieConst {
+    const val list_item = 1
+    const val tile_item = 2
+}
+
 class MovieListAdapter : RecyclerView.Adapter<MovieListAdapter.MovieViewHolder>(){
+    private val tag = "MovieListAdapter"
+    private var vType = MovieConst.list_item
     var movieList = listOf<Movie>()
 
     fun updateMovieList(newMovieList: List<Movie>?) {
@@ -16,33 +24,51 @@ class MovieListAdapter : RecyclerView.Adapter<MovieListAdapter.MovieViewHolder>(
         notifyDataSetChanged()
     }
 
+    fun setViewType(type: Int){vType = type}
+
     override fun getItemCount() = movieList.size
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): MovieViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
+        var itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.movie_list_item, parent, false)
-        return MovieViewHolder(itemView)
+
+        // set which layout file to use
+        when (vType) {
+            MovieConst.list_item -> itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.movie_list_item, parent, false)
+            MovieConst.tile_item -> itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.movie_tile_item, parent, false)
+        }
+        return MovieViewHolder(itemView, vType)
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         holder.bind(movieList[position])
     }
 
-    class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class MovieViewHolder(itemView: View, vType: Int) : RecyclerView.ViewHolder(itemView) {
         private val title: TextView = itemView.findViewById(R.id.tv_title)
-        private val overview: TextView = itemView.findViewById(R.id.tv_overview)
+        private val overview: TextView? = itemView.findViewById(R.id.tv_overview)
         private val release_date: TextView = itemView.findViewById(R.id.tv_release_date)
-        private val popularity: TextView = itemView.findViewById(R.id.tv_popularity)
-
+        private val popularity: TextView? = itemView.findViewById(R.id.tv_popularity)
+        private val vType:Int by lazy { vType }
 
         fun bind(movie: Movie) {
-            title.text = movie.title
-            overview.text = movie.overview
-            release_date.text = movie.release_date
-            popularity.text = movie.popularity.toString()
+            when (vType) {
+                MovieConst.list_item -> {
+                    title.text = movie.title
+                    release_date.text = movie.release_date
+                    overview!!.text = movie.overview
+                    popularity!!.text = movie.popularity.toString()
+                }
+                MovieConst.tile_item ->{
+                    title.text = movie.title
+                    release_date.text = movie.release_date
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/movietime/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/example/movietime/ui/library/LibraryFragment.kt
@@ -5,19 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movietime.R
 import com.example.movietime.data.Movie
 import com.example.movietime.databinding.FragmentLibraryBinding
+import com.example.movietime.ui.home.MovieConst
 import com.example.movietime.ui.home.MovieListAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class LibraryFragment : Fragment() {
 
     private var _binding: FragmentLibraryBinding? = null
     private val movieAdapter = MovieListAdapter()
     private lateinit var searchResultsListRV: RecyclerView
+    private lateinit var fab: FloatingActionButton
+    private var isList:Boolean = true
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,19 +33,37 @@ class LibraryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(LibraryViewModel::class.java)
-
         _binding = FragmentLibraryBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         searchResultsListRV = root.findViewById(R.id.rv_library_list)
-        searchResultsListRV.layoutManager = LinearLayoutManager(root.context)
-        searchResultsListRV.adapter = movieAdapter
-        movieAdapter.updateMovieList(listOf(Movie("Shrek is an anti-social and highly-territorial green ogre who loves the solitude of his swamp. His life is interrupted after the dwarfish Lord Farquaad of Duloc unknowingly exiles a vast number of fairy-tale creatures to Shrek's swamp.",93.0,"2000","Shrek")))
+        fab = root.findViewById(R.id.ab_sort)
+
+
+        setView(root)
+        movieAdapter.updateMovieList(listOf(Movie("Shrek is an anti-social and highly-territorial green ogre who loves the solitude of his swamp. His life is interrupted after the dwarfish Lord Farquaad of Duloc unknowingly exiles a vast number of fairy-tale creatures to Shrek's swamp.",93.0,"2000","Shrek"),
+            Movie("Shrek is an anti-social and highly-territorial green ogre who loves the solitude of his swamp. His life is interrupted after the dwarfish Lord Farquaad of Duloc unknowingly exiles a vast number of fairy-tale creatures to Shrek's swamp.",93.0,"2000","Shrek"),
+            Movie("Shrek is an anti-social and highly-territorial green ogre who loves the solitude of his swamp. His life is interrupted after the dwarfish Lord Farquaad of Duloc unknowingly exiles a vast number of fairy-tale creatures to Shrek's swamp.",93.0,"2000","Shrek")))
+
+        fab.setOnClickListener { setView(root) }
 
         return root
     }
+
+    private fun setView(root: View){ //TODO add some kind of animation
+        if (isList){
+            searchResultsListRV.layoutManager = LinearLayoutManager(root.context)
+            movieAdapter.setViewType(MovieConst.list_item)
+            isList = false
+            fab.setImageResource(R.drawable.ic_baseline_view_column_24)
+        }else{
+            searchResultsListRV.layoutManager = GridLayoutManager(root.context, 2)
+            movieAdapter.setViewType(MovieConst.tile_item)
+            isList = true
+            fab.setImageResource(R.drawable.ic_baseline_view_list_24)
+        }
+        searchResultsListRV.adapter = movieAdapter
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
