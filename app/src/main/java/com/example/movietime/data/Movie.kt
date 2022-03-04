3973fe2a2a5d991b41302aba0643be3d026db2ef
Erik-Handeland <erikhandeland@gmail.com>
2022-03-03 18:45:42 -0800

    Moved movie stuff to API folder, and added poster images

--- app/src/main/java/com/example/movietime/data/Movie.kt
+++ app/src/main/java/com/example/movietime/data/Movie.kt
@@ -2,6 +2,10 @@ package com.example.movietime.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+object API_Const {
+    const val img_base_url = "https://image.tmdb.org/t/p/original/"
+}
+
 @Entity
 data class Movie(
     //val budget: Int?,
@@ -11,8 +15,8 @@ data class Movie(
     //val id: Int, //
 
     //val runtime: Int, // int or null
-    //val backdrop_path: String, // string or null
-    //val poster_path: String, // string or null*/
+    val backdrop_path: String?,
+    val poster_path: String?,
     @PrimaryKey
     val overview: String, // string or null
     //val original_title: String,
