79d328d0bbb071c083e4168631fe23d5d6a75679
kimtaes <67464984+kimtaes@users.noreply.github.com>
2022-03-07 23:25:06 -0800

    added a little change on DB

--- app/src/main/java/com/example/movietime/data/Movie.kt
+++ app/src/main/java/com/example/movietime/data/Movie.kt
@@ -1,23 +1,19 @@
 package com.example.movietime.data
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity
+import java.io.Serializable
+
+object API_Const {
+    const val img_base_url = "https://image.tmdb.org/t/p/original/"
+}
+
 data class Movie(
-    //val budget: Int?,
-    //val genres: Int,
-
-    //@PrimaryKey
-    //val id: Int, //
-
-    //val runtime: Int, // int or null
-    //val backdrop_path: String, // string or null
-    //val poster_path: String, // string or null*/
-    @PrimaryKey
-    val overview: String, // string or null
-    //val original_title: String,
+    val id: Int,
+    val title: String,
+    val original_title: String,
+    val overview: String,
     val popularity: Double,
     val release_date: String?,
-    //val status: String,
-    val title: String
-)
+    val poster_path: String?,
+    val backdrop_path: String?,
+    val genre_ids: List<Int>,
+) : Serializable
