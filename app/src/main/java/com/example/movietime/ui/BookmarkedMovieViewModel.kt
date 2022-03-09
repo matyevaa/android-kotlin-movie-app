5de519a01eb36164844d50290c2ca1656295dd66
kimtaes <67464984+kimtaes@users.noreply.github.com>
2022-03-08 19:04:01 -0800

    notification and DB

---
+++ app/src/main/java/com/example/movietime/ui/BookmarkedMovieViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.movietime.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.movietime.data.AppDatabase
+import com.example.movietime.data.BookmarkedMovie
+import com.example.movietime.data.DetailedMovie
+import kotlinx.coroutines.launch
+
+class BookmarkedMovieViewModel(application: Application): AndroidViewModel(application) {
+
+    private val detailedMovie = BookmarkedMovie(
+        AppDatabase.getInstance(application).MovieDao()
+    )
+
+    val bookmarkedMovies = detailedMovie.getAllDetailedMovie().asLiveData()
+    fun addDetailedMovie(movie: DetailedMovie){
+        viewModelScope.launch {
+            detailedMovie.insertMovie(movie)
+        }
+
+    }
+
+    fun deleteDetailedMovie(movie:DetailedMovie){
+        viewModelScope.launch {
+            detailedMovie.deleteMovie(movie)
+        }
+    }
+
+}
