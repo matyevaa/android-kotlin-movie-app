3a1382eae31afd1b2b013fa951783f531f2ead4c
Erik-Handeland <erikhandeland@gmail.com>
2022-03-09 20:28:11 -0800

    Added Google auth

--- app/src/main/java/com/example/movietime/MainActivity.kt
+++ app/src/main/java/com/example/movietime/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.movietime
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,11 +13,16 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.movietime.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationView
+import com.example.movietime.ui.profile.LoginStatus.account
+import com.example.movietime.ui.profile.LoginStatus.isLoggedIn
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var appBarConfig: AppBarConfiguration
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,9 +56,23 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_profile -> {
                 findNavController(R.id.nav_host_fragment).navigate(R.id.navigation_sign_in)
+                //Feel free to remove this ^ when adding in the profile page, i'll just tack on
+                // a sign out button when done
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        Log.d(TAG, "Account: " + acct.toString())
+        isLoggedIn = acct != null
+        account = acct
+        if(!isLoggedIn){
+            //findNavController(R.id.nav_host_fragment).navigate(R.id.navigation_sign_in)
+            // ^ Uncomment to try sign on with google
         }
     }
 }
