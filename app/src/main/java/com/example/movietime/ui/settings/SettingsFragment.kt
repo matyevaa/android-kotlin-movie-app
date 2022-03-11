06efc9e351b416ee65bf6a3cd36dbd0ee62356a6
Timothy Nguyen <timothynguyen2000@yahoo.com>
2022-03-11 14:48:11 -0800

    Initialized files related to Settings and Stats

--- app/src/main/java/com/example/movietime/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/movietime/ui/settings/SettingsFragment.kt
@@ -1,4 +1,12 @@
 package com.example.movietime.ui.settings
 
-class SettingsFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.movietime.R
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- app/src/main/java/com/example/movietime/ui/stats/StatsFragment.kt
+++ app/src/main/java/com/example/movietime/ui/stats/StatsFragment.kt
@@ -1,4 +1,12 @@
 package com.example.movietime.ui.stats
 
-class StatsFragment {
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.movietime.R
+
+class StatsFragment : Fragment(R.layout.fragment_stats) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
