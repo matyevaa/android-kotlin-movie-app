df11f31736b5d65aded5bc492eceed56b48c7932
Timothy Nguyen <timothynguyen2000@yahoo.com>
2022-03-13 20:43:01 -0700

    Added Dark Mode switch to Settings. Unable to retain switch state after leaving Settings

--- app/src/main/java/com/example/movietime/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/movietime/ui/settings/SettingsFragment.kt
@@ -1,12 +1,31 @@
 package com.example.movietime.ui.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.example.movietime.R
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val dark_button = view.findViewById<Switch>(R.id.switch_dark_mode)
+
+        dark_button.setOnCheckedChangeListener { _, isChecked ->
+            if(dark_button.isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                dark_button.text = "Disable Dark Mode"
+            }
+            else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                dark_button.text = "Enable Dark Mode"
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/movietime/ui/settings/SettingsViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.movietime.ui.settings
-
-class SettingsViewModel {
-}
