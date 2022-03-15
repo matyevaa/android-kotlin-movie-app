def8599c2fcdf83921f740790aa7ee9c2f0176b9
Erik-Handeland <erikhandeland@gmail.com>
2022-03-14 19:41:41 -0700

    Room hooked up

--- app/src/main/java/com/example/movietime/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/movietime/ui/settings/SettingsFragment.kt
@@ -2,10 +2,14 @@ package com.example.movietime.ui.settings
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.example.movietime.R
+import kotlin.math.log
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -15,7 +19,32 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.setBackgroundColor(Color.parseColor("#FFFFFF"))
+
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        var darkmode = sharedPrefs.getBoolean(getString(R.string.darkmode), true)
+
+        if(darkmode){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            view.setBackgroundColor(Color.parseColor("#21FFFFFF"))
+
+        }
+        else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            view.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        }
+
+        sharedPrefs.registerOnSharedPreferenceChangeListener{ pref, key ->
+            val mode = pref.getBoolean(key, true)
+            if(mode){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                view.setBackgroundColor(Color.parseColor("#21FFFFFF"))
+
+            }
+            else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                view.setBackgroundColor(Color.parseColor("#FFFFFF"))}
+        }
+
         super.onViewCreated(view, savedInstanceState)
     }
 }
